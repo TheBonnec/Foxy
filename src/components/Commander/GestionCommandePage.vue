94155621270861526aeb1e71b99c9e23d0cec3a4
TheBonnec <thomaslebonnec@icloud.com>
2023-03-19 11:58:03 +0100

    Organisation et hierarchisation des pages + navigation primaire

---
+++ src/components/Commander/GestionCommandePage.vue
@@ -0,0 +1,109 @@
+<template>
+    <div class="container">
+        <header>
+            <h1>Commander la création d'un logo</h1>
+            <p>Veuillez choisir ci-dessous</p>
+        </header>
+
+
+        <section>
+            <div class="card">
+                <h2>Commander un logo</h2>
+                <p>Remplir le formulaire de commande</p>
+                <button class="orange" @click="commanderHit">Commander Maintenant</button>
+            </div>
+
+            <div class="card">
+                <h2>Retrouver une commande</h2>
+                <p>Retrouver une commande enregistré</p>
+                <button class="noir">Retrouver Maintenant</button>
+            </div>
+        </section>
+    </div>
+</template>
+
+
+
+
+<script>
+export default {
+    methods: {
+        commanderHit() {
+            this.$emit('show-formulaire', true);
+        }
+    }
+}
+</script>
+
+
+
+
+
+<style scoped>
+h1 {
+    font-family: Araboto-Bold;
+    font-size: 1.5rem;
+    text-decoration: none;
+    margin: 0;
+}
+
+h2 {
+    font-weight: 900;
+    font-size: 1.3rem;
+    text-decoration: none;
+    color: #878FA0;
+    margin: 0;
+}
+
+p {
+    font-size: 1rem;
+    font-weight: 500;
+    color: #878FA0;
+    margin: 0;
+}
+
+
+header {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
+section {
+    display: flex;
+    justify-content: center;
+    margin: 2rem;
+    gap: 2rem;
+}
+
+.card {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    text-align: center;
+    min-width: 300px;
+    border-style: solid;
+    border-width: 1px;
+    border-color: #e4e4e4;
+    border-radius: 4px;
+    padding: 1rem;
+}
+
+button {
+    border: none;
+    padding: 1rem;
+    font-size: 1rem;
+    color: #ffffff;
+    margin-top: 1rem;
+}
+
+.orange {
+    background-color: #F7931A;
+}
+
+.noir {
+    background-color: #343A3E;
+}
+
+
+</style>
---
+++ src/components/General/TopBar.vue
@@ -0,0 +1,65 @@
+<template>
+    <div class="container">
+        <div id="telephone" class="content-group">
+            <img alt="Logo telephone" src="../../assets/tel.png">
+            <p>+33 (0)5 35 54 64 96 - +33 (0)7 80 91 95 10</p>
+        </div>
+        
+        <div id="right-div">
+            <div class="content-group">
+                <img alt="Logo connexion" src="../../assets/connexion.png">
+                <p>Connexion</p>
+            </div>
+            <div class="content-group">
+                <img alt="Logo inscription" src="../../assets/inscription.png">
+                <p>Inscription</p>
+            </div>
+            <div class="content-group">
+                <img alt="Logo avis client" src="../../assets/avis_clients.png">
+                <p>Avis client</p>
+            </div>
+        </div>
+    </div>
+    <hr />
+</template>
+
+
+
+
+<style scoped>
+img {
+    max-width: 20px;
+    max-height: 20px;
+}
+
+.container {
+    display: flex;
+    align-items: center;
+}
+
+.content-group {
+    display: flex;
+    align-items: center;
+    gap: 0.4rem;
+}
+
+#telephone {
+    order: 1;
+}
+
+#right-div {
+    order: 2;
+    display: flex;
+    justify-content: flex-end;
+    margin-left: auto;
+    gap: 0.8rem;
+}
+
+hr {
+    border-style: solid;
+    border-width: 0.01rem;
+    border-color: #f2f2f2;
+    margin: 0;
+}
+
+</style>
